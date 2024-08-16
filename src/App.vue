af43f83787d8ce317934b58c4d3dfb8d59893883
digest77-ks <digest77@hanmail.net>
2024-08-16 16:33:21 +0900

    9 appbar  만들기

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <v-app>
+      <v-container>
+        <v-app-bar color='primary' dark >
+          <v-app-bar-nav-icon/>
+          <v-app-bar-title>Page Bar</v-app-bar-title>
+          <v-spacer/>
+          <v-btn icon to='/about'>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+          <v-btn icon to='/'>
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+      </v-app-bar>
+      </v-container>
+      <v-main>
+             <router-view/>
+      </v-main>
+    </v-app>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  }
+
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/xxx',
+    name: 'xxx',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/xxxView.vue')
   }
 ]
 
